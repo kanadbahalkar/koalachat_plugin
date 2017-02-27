77c385163b7d72e2a5993f1744833dafc1a2fb3b
Kanad Bahalkar <kanad@koalachat.com>
2017-02-27 18:39:33 -0500

    Updated Plugin

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   //Create iframe for chat
   var iframe = document.createElement('iframe');
-  iframe.style = 'border: none; position: fixed; right: 0; bottom: 0; width: 100%; height: 100%; z-index: 9123479812';
-  iframe.src = 'https://s3.amazonaws.com/koalachat/plugin.html';
-  iframe.id = 'plugin';
+  iframe.style = 'border: none; position: fixed; right: 0; bottom: 0; z-index: 9123479812';
+  // iframe.src = 'https://s3.amazonaws.com/koalachat/plugin.html';
+  iframe.src = '../../plugin.html';
+  iframe.name = window.parent.document.getElementById('koala-index').getAttribute('u'); 
+  iframe.id = 'koala-plugin';
   document.body.appendChild(iframe);
 }
 
