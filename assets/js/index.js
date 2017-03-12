c20e9223729dc3d2e9410b274b0294089a8ab73b
Kanad <kandyrox@gmail.com>
2017-03-12 12:39:33 -0400

    Updated CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,8 @@
 window.onload = function() {
   //Create iframe for chat
   var iframe = document.createElement('iframe');
-  iframe.style = 'border: none; position: fixed; right: 0; bottom: 0; z-index: 9123479812';
-  // iframe.src = 'https://s3.amazonaws.com/koalachat/plugin.html';
-  iframe.src = '../../plugin.html';
+  iframe.style = 'width: 340px; height: 400px; max-height: 100%; border: none; position: fixed; right: 0; bottom: 0; z-index: 9123479812';
+  iframe.src = 'https://s3.amazonaws.com/koalachat/plugin.html';
   iframe.name = window.parent.document.getElementById('koala-index').getAttribute('u'); 
   iframe.id = 'koala-plugin';
   document.body.appendChild(iframe);
